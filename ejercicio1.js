731bfe7baef7176b7b51004926806030803912a7
oscarzafra <oscarzafra35@gmail.com>
2024-05-20 12:14:06 +0200

    Ejercicios 1 y 2

---
+++ ejercicio1.js
@@ -0,0 +1,23 @@
+// Define tu superhéroe favorito
+var miSuperheroeFavorito = "Hulk";
+
+
+// Define un número
+let numeroFavorito = 50;
+
+
+// Define las dimensiones
+let altura = 5;
+let longitud = 10;
+
+
+// Calcula la suma
+let suma = altura + longitud;
+
+
+// Imprime los resultados
+console.log(miSuperheroeFavorito); 
+console.log(numeroFavorito); 
+console.log(altura);
+console.log(longitud);
+console.log(suma);
---
+++ ejercicio2.js
@@ -0,0 +1,40 @@
+// Ejercicio 2.1
+const jedi = {nombre: "Luke Skywalker", edad: 19};
+
+console.log(jedi.edad);
+
+jedi.edad = 25;
+
+console.log(jedi.edad);
+
+
+
+// Ejercicio 2.2
+let nombre = "Leia";
+let apellido = "Organa"
+let edad = 20;
+
+let mensaje = "Soy "+ nombre +" "+ apellido +", tengo "+ edad +" años y soy una princesa de Alderaan.";
+
+console.log (mensaje);
+
+
+
+// Ejercicio 2.3
+const sable1 = {nombre: "Shoto de Yoda", precio: 1500};
+const sable2 = {nombre: "Sable de Darth Vader", precio: 2000};
+
+let suma = sable1.precio + sable2.precio;
+
+console.log(suma);
+
+
+
+// Ejercicio 2.4
+let precioBaseGlobal = 10000;
+
+const nave1 = {nombre: "Ala-X", precioBase: 50000, precioFinal: 60000};
+const nave2 = {nombre: "Halcón Milenario", precioBase: 70000, precioFinal: 80000};
+
+
+
