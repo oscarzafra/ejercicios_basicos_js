a20d20c465ce09500fc913d606328b7432ec1f3d
oscarzafra <oscarzafra35@gmail.com>
2024-05-23 10:13:21 +0200

    Ejercicio 5 Completado

---
+++ ejercicio6.js
@@ -0,0 +1,10 @@
+
+1.1 Crea un bucle for que vaya desde 0 a 9 y muestra el valor de i por consola.
+
+1.2 Crea un bucle for que vaya desde 0 a 9 y muestra el valor de i por consola solo 
+cuando el resto del numero dividido entre 2 sea 0.
+
+1.3 Crea un bucle para conseguir dormir contando ovejas. 
+Este bucle tiene que dar 10 vueltas, es decir, 10 console.log.
+Muestra por consola un mensaje diciendo 'Intentando dormir 🐑' en cada vuelta del bucle 
+y cambia el mensaje en la décima vuelta a '¡Dormido!'.
